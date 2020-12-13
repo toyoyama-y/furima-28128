8321085e37ee4ff2162ec3a9cef5a7afb716bbe4
toyoyama-y <69073666+toyoyama-y@users.noreply.github.com>
2020-12-13 23:04:39 +0900

    destroyアクションの分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render 'show'
+    end
   end
 
   def update
