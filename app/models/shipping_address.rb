e1415e91f739959db40998737b4253093eb284a1
toyoyama-y <69073666+toyoyama-y@users.noreply.github.com>
2020-12-17 21:16:47 +0900

    アソシエーション

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,4 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :wallet
+
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,7 @@
 class Wallet < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one :shipping_address
+
 end
 
