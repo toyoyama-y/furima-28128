89d37ebcc83b167463df039d56f8f0bf2453420c
toyoyama-y <69073666+toyoyama-y@users.noreply.github.com>
2020-12-18 20:19:12 +0900

    formオブジェクトのバリデーション

--- app/models/buyer_form.rb
+++ app/models/buyer_form.rb
@@ -3,8 +3,12 @@ class Buyer
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :telephone
 
-  def save
-
+  with_options presence: true do
+    validates :postal_code, format: {with: /^\d{3}[-]\d{4}$/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :telephone,  format: {with: /^\d{11}$/, message: "is invalid. Do not include hyphen(-)"}
   end
 
 end
