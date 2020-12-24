fbbd5f3e7b8cdc586cba6273ac41653771b59f71
toyoyama-y <69073666+toyoyama-y@users.noreply.github.com>
2020-12-24 20:07:44 +0900

    購入ページの直打ち対策

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index, :create]
+
+
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
