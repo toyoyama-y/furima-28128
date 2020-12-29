90a82f1b09754f6442f82910e7efee91fb0f432e
toyoyama-y <69073666+toyoyama-y@users.noreply.github.com>
2020-12-29 15:01:28 +0900

    購入ページのエラーハンドリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,24 @@ class OrdersController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+    @buyer_form = BuyerForm.new
   end
 
   def create
-  end 
+    @item = Item.find(params[:item_id])
+    @buyer_form = BuyerForm.new(order_params)
+    if @buyer_form.valid?
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:buyer_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :telephone).merge(user_id: current_user.id , item_id: params[:item_id])
+  end
+
 
 end
--- app/models/buyer_form.rb
+++ app/models/buyer_form.rb
@@ -1,19 +1,19 @@
-class Buyer
+class BuyerForm
   
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :telephone
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :telephone, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /^\d{3}[-]\d{4}$/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id
     validates :city
     validates :address
-    validates :telephone,  format: {with: /^\d{11}$/, message: "is invalid. Do not include hyphen(-)"}
+    validates :telephone,  format: {with: /\A\d{11}\z/, message: "is invalid. Do not include hyphen(-)"}
   end
 
   def save
     # 購入者情報を保存
-    wallet = Wallet.create(user_id: user.id, item_id: item.id)
+    wallet = Wallet.create(user_id: user_id, item_id: item_id)
     # 配送先情報を保存
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, telephone: telephone, wallet_id: wallet.id)
   end
